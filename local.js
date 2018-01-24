8df4e169ffe6ad2ab024dbed9c27de370b29bac6
unknown <pablorodriguezgarc@gmail.com>
2018-01-24 19:49:31 +0100

    Formulario con local Storage

---
+++ local.js
@@ -0,0 +1,26 @@
+  let listado=[];
+
+if (localStorage.getItem('listado')){
+   listado=JSON.parse(localStorage.getItem('listado'));
+   for (item of listado){
+      $('ul').append('<li>'+item+'</li>');
+   }
+}
+
+$('#envio').click(function(e){
+  e.preventDefault();
+  let datos=$('#item1').val()+' '+$('#item2').val()+' '+$('#item3').val()+' '+$('#item4').val();
+  $('ul').append('<li>'+datos+'</li>');
+  $('#item1').val('');
+  $('#item2').val('');
+  $('#item3').val('');
+  $('#item4').val('');
+  listado.push(datos);
+  localStorage.setItem('listado', JSON.stringify(listado));
+});
+
+$('#clear').click(function(e){
+  $('ul').empty('listado');
+  localStorage.removeItem('listado');
+  $('h2').html('</br>El texto ha sido limpiado');
+})
